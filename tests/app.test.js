4fe629b94cc1d30ffdd3957d56fdc4f116615ec0
mpl4042 <mimite4040@yahoo.com>
2026-09-08 00:38:43 -0300

    Create app.test.js

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('Testes da API', () => {
+    it('Deve retornar status 200 e a mensagem de sucesso na raiz', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.status).toBe("API rodando com sucesso!");
+    });
+});
