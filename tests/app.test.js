8c58f99776f163c3088ae4a859f755488fc0e513
mpl4042 <mimite4040@yahoo.com>
2026-09-08 00:45:44 -0300

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ describe('Testes da API', () => {
     it('Deve retornar status 200 e a mensagem de sucesso na raiz', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.body.status).toBe("Texto Erradoooo!");
+        expect(res.body.status).toBe("API rodando com sucesso!");
     });
 });
